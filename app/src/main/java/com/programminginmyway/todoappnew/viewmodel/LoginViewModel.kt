9847716adf461a7048281be997ccc50f5d6d7f48
RavindraMandale <ravindra@visio-apps.com>
2025-10-28 19:34:14 +0530

    code clean

--- app/src/main/java/com/programminginmyway/todoappnew/viewmodel/LoginViewModel.kt
+++ app/src/main/java/com/programminginmyway/todoappnew/viewmodel/LoginViewModel.kt
@@ -24,7 +24,6 @@ class LoginViewModel(application: Application) : AndroidViewModel(application) {
     // LiveData for showing Toast messages in the UI
     val snackBarMessage = SingleLiveEvent<Int>() // Using Int for String resource ID
 
-    private val context = application.applicationContext
     private val userDao = AppDatabase.getDatabase(application).userDao()
 
     fun validateAndLogin() {
@@ -37,8 +36,6 @@ class LoginViewModel(application: Application) : AndroidViewModel(application) {
             }
 
             !Patterns.EMAIL_ADDRESS.matcher(emailValue).matches() -> {
-                //Toast.makeText(context, R.string.enter_valid_email_address, Toast.LENGTH_SHORT)
-                  //  .show()
                 snackBarMessage.value = R.string.enter_valid_email_address
             }
 
@@ -53,28 +50,11 @@ class LoginViewModel(application: Application) : AndroidViewModel(application) {
     }
 
     private fun checkLogin(email: String, password: String) {
-//        val editor = prefs.edit()
-//        var found = false
-//        val userList: List<Users> = dbHelper.checkAllUsers()
-//        for (user in userList) {
-//            if (user.emailId == email && user.password == password) {
-//                found = true
-//                Toast.makeText(context, R.string.login_success_message, Toast.LENGTH_SHORT).show()
-//                editor.putBoolean(LoginScreen.USER_LOGIN_KEY, true)
-//                editor.apply()
-//                loginSuccess.postValue(true)
-//                break
-//            }
-//        }
-//        if (!found) {
-//            Toast.makeText(context, R.string.login_failed_message, Toast.LENGTH_SHORT).show()
-//        }
         viewModelScope.launch {
             val user = withContext(Dispatchers.IO) {
                 userDao.getUserByEmail(email)
             }
             if (user != null && PasswordUtils.isPasswordValid(password, user.PASSWORD)) {
-                //Toast.makeText(context, R.string.login_success_message, Toast.LENGTH_SHORT).show()
                 snackBarMessage.value = R.string.login_success_message
                 //prefs.edit { putBoolean(LoginScreen.USER_LOGIN_KEY, true) }
                 val appContext = getApplication<Application>()
@@ -84,7 +64,6 @@ class LoginViewModel(application: Application) : AndroidViewModel(application) {
                 loginSuccess.postValue(true)
             } else {
                 snackBarMessage.value = R.string.login_failed_message
-                //Toast.makeText(context, R.string.login_failed_message, Toast.LENGTH_SHORT).show()
             }
         }
     }
